2e333bdc1ba59fc1f8d869cd1184331c9eb5aeef
MoonHyang-gi <ansgidrl3@gmail.com>
2024-05-12 02:36:44 +0900

    Feat: Player Move

---
+++ Assets/Scripts/TopDownRotation.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class TopDownRotation : MonoBehaviour
+{
+    [SerializeField] private SpriteRenderer characterRenderer;
+
+    private TopDownController controller;
+
+    private void Awake()
+    {
+        controller = GetComponent<TopDownController>();
+    }
+
+    private void Start()
+    {
+        controller.OnLookEvent += OnAim;
+    }
+
+    public void OnAim(Vector2 direction)
+    {
+        RotateCharacter(direction);
+    }
+
+    private void RotateCharacter(Vector2 direction)
+    {
+        float rotZ = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        characterRenderer.flipX = Mathf.Abs(rotZ) > 90f;
+    }
+}
